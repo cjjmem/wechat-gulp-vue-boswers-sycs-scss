e196e53d9ab5e26a3d8c4848dc2d71981649bd2c
chenjj <chenjj@yunhuni.com>
2016-11-10 15:44:45 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var minify = require('gulp-minify-css')
+
+
+gulp.task('default',function(){
+
+  return gulp.src('assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minify())
+    .pipe(gulp.dest('assets/css'));
+
+
+})
+
