8fc6fd5bd6e76851a639fbe77d56871d96e878cd
chenjj <chenjj@yunhuni.com>
2016-11-12 10:58:54 +0800

    添加 gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var minify = require('gulp-minify-css')
+var watch = require('gulp-watch')
 
 
 gulp.task('default',function(){
-  return gulp.src('assets/scss/**/*.scss')
+
+
+/*  return gulp.src('assets/scss/!**!/!*.scss')
     .pipe(sass().on('error', sass.logError))
     //.pipe(minify())
-    .pipe(gulp.dest('assets/css'));
+    .pipe(gulp.dest('assets/css'));*/
+
+  return watch('assets/scss/**/*.scss',function(){
+    gulp.src('assets/scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      //.pipe(minify())
+      .pipe(gulp.dest('assets/css'));
+  });
+
 })
 
